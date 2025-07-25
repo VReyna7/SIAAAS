9e60177d7e295e4343b5de7c6103ef1847ded0ed
Victor Reyna <veduardo239@gmail.com>
2025-07-25 10:46:00 -0600

    clases de entidad / models.py

---
+++ file_manager/models.py
@@ -0,0 +1,54 @@
+import datetime
+
+class Directory:
+    def __init__(self, name, path, date_create, timestamp):
+        self.__name = name
+        self.__path = path
+        self.__date_create = date_create
+        self.__date_update = timestamp
+
+class File: 
+    def __init__(self, name, ext, path, size, timestamp):
+        self.__name = name
+        self.__ext = ext
+        self.__path = path
+        self.__size = size
+        self.__timestap = timestamp 
+
+class Event: 
+    def __init__(self, description, tipo, timestamp:datetime):
+        self.__tipo = tipo
+        self.__descripcion = description
+        self.__timestamp : datetime = timestamp
+
+    def to_log_string(self):
+        return f"[{self.__timestamp.isoformat()} {self.__tipo} {self.__descripcion}]"
+
+class ErrorEvent (Event):
+    def __init__(self, timestamp, description, stack_trace, error_message):
+        self.__error_message = error_message
+        self.__stack_trace = stack_trace
+        full_description = (
+            f"{description} | Error: {error_message} | "
+            f"Trace: {stack_trace[:100].replace('\n', ' ')}..."
+        )
+        super().__init__(full_description, 'ERROR', timestamp)
+    
+    @property
+    def error_message(self):
+        return self.__error_message
+
+    @error_message.setter
+    def error_message(self, nuevo_valor):
+        if not nuevo_valor.strip():
+            raise ValueError("El error no puede estar vacio")
+        self.__error_message = nuevo_valor
+
+
+class OperationEvent (Event):
+    def __init__(self, timestamp, description, affected_path, operation_type):
+        self.__operation_type = operation_type
+        self.__affected_path = affected_path
+        full_description = f"{description} | Operation: {operation_type} | Path: {affected_path}"
+        super().__init__(full_description, 'FILE_OPERATION', timestamp)
+    
